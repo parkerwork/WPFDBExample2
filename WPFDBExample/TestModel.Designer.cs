ec308d1e90ac958c3b71593a9e6617a02f24af8f
Sean Wireman <seanwireman@hotmail.com>
2016-04-08 14:00:07 -0400

    EF added

--- WPFDBExample/TestModel.Designer.cs
+++ WPFDBExample/TestModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\sfwirema\Source\Repos\WPFDBExample2\WPFDBExample\TestModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\zzsfwirema\Source\Repos\wpfdbexample2\WPFDBExample\TestModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
